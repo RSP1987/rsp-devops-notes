528ba5c2c877d565a3545d9bc09faff11228769c
Rajasekhar <grajasekhar.devops@gmail.com>
2026-03-24 11:59:40 +0530

    added code

---
+++ docusaurus.config.js
@@ -0,0 +1,16 @@
+const config = {
+  title: 'My Site',
+  tagline: 'DevOps Notes',
+
+  url: 'https://RSP1987.github.io',
+  baseUrl: '/rsp-devops-notes/',
+
+  organizationName: 'RSP1987',
+  projectName: 'rsp-devops-notes',
+  deploymentBranch: 'gh-pages',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  favicon: 'img/favicon.ico',
+};
